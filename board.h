2cca2c8c962270ee868b50a8662b6af3e2067261
ethanchitwoodcedar <ethanchitwood@cedarville.edu>
2024-11-04 15:54:16 -0500

    updated board.h

--- board.h
+++ board.h
@@ -1,15 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 #define BOARD_SIZE 9
 #define MINES 10
+
+using namespace std;
 
 struct board {
    char front[BOARD_SIZE][BOARD_SIZE];
    char back[BOARD_SIZE][BOARD_SIZE];
 };
 
-void printBoard() {
-  
+void printBoard(board& master) {
+   system("clear");
+
+   for(int i = 0; i < BOARD_SIZE; i++) {
+      for(int j = 0; j < BOARD_SIZE; j++) {
+         if(master.front[i][j] == '+') {
+            cout << '+';
+         }
+         else {
+            cout << master.back[i][j];
+         }
+      }
+      cout << endl;
+   }
 }
 
-void buildBoard() {
-  
+void buildBoard(board& master, int posX, int posY) {
+   int x;
+   int y;
+   srand(time(0));
+
+   for(int i = 0; i < BOARD_SIZE; i++) {
+      x = rand() % BOARD_SIZE;
+      y = rand() % BOARD_SIZE;
+      if((x >= (posX - 1)) && (x <= (posX + 1)) && (y >= (posY - 1)) && (y <= (posY + 1))) {
+         i--;
+         continue;
+      }
+      if(master.back[x][y] != '*') {
+         master.back[x][y] = '*';
+      }
+      else {
+         i--;
+      }
+   }
+
+   for(int i = 0; i < BOARD_SIZE; i++) {
+      for(int j = 0; j < BOARD_SIZE; j++) {
+         master.front[i][j] = '+';
+      }
+   }
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-
-#define BOARD_SIZE 9
-#define MINES 10
+#include "board.h"
 
 using namespace std;
 
 int main() {
-   
+   int posX  = 4;
+   int posY = 4;
+
+   board master;
+   buildBoard(master, posX, posY);
+   printBoard(master);
 }
