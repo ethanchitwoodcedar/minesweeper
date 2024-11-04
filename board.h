50da9eb643cbf329aeb4c725bf9ff558333bfc29
ethanchitwoodcedar <ethanchitwood@cedarville.edu>
2024-11-04 14:48:38 -0500

    Create board.h

---
+++ board.h
@@ -0,0 +1,15 @@
+#define BOARD_SIZE 9
+#define MINES 10
+
+struct board {
+   char front[BOARD_SIZE][BOARD_SIZE];
+   char back[BOARD_SIZE][BOARD_SIZE];
+};
+
+void printBoard() {
+  
+}
+
+void buildBoard() {
+  
+}
